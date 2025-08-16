d62c937e7dd7699e6585c2ab2a8b4de44986997d
Saud <qsaudhussain@gmail.com>
2025-08-16 14:25:17 +0530

    Update productRoutes.js

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,9 +1,29 @@
+// import express from "express";
+// import {
+//   uploadProduct,
+//   getAllProducts,
+//   getProductById,
+//   deleteProduct,
+//   upload,
+// } from "../controllers/productController.js";
+// import { protect } from "../middleware/authMiddleware.js";
+
+// const router = express.Router();
+
+// router.post("/", upload, uploadProduct);
+// router.get("/", getAllProducts);
+// router.get("/:id", getProductById);
+// router.delete("/:id", protect, deleteProduct);
+
+// export default router;
+
 import express from "express";
 import {
   uploadProduct,
   getAllProducts,
   getProductById,
   deleteProduct,
+  updateProductStatus,
   upload,
 } from "../controllers/productController.js";
 import { protect } from "../middleware/authMiddleware.js";
@@ -14,5 +34,6 @@ router.post("/", upload, uploadProduct);
 router.get("/", getAllProducts);
 router.get("/:id", getProductById);
 router.delete("/:id", protect, deleteProduct);
+router.put("/:id/status", updateProductStatus); // Added
 
 export default router;
