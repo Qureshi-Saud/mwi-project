ae12e19399eefd5d4d964551124ae62291cbed3d
Saud <qsaudhussain@gmail.com>
2025-08-16 14:24:54 +0530

    Update Product.js

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,3 +1,30 @@
+// import mongoose from "mongoose";
+
+// const productSchema = new mongoose.Schema({
+//   name: { type: String, required: true },
+//   type: { type: String, required: true },
+//   application: { type: String, required: true },
+//   image: { type: String, required: true },
+//   image2: { type: String }, // New field
+//   materials: {
+//     sealRingFaces: String,
+//     seatFaces: String,
+//     elastomer: String,
+//     moc: String,
+//     bellowMoc: String,
+//     endFittingMoc: String,
+//   },
+//   operatingLimits: {
+//     shaftDia: String,
+//     pressure: String,
+//     temperature: String,
+//     speed: String,
+//   },
+// }, { timestamps: true });
+
+// const Product = mongoose.model("Product", productSchema);
+// export default Product;
+
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
@@ -5,7 +32,8 @@ const productSchema = new mongoose.Schema({
   type: { type: String, required: true },
   application: { type: String, required: true },
   image: { type: String, required: true },
-  image2: { type: String }, // New field
+  image2: { type: String },
+  status: { type: String, enum: ["Active", "Inactive"], default: "Active" }, // Added status
   materials: {
     sealRingFaces: String,
     seatFaces: String,
