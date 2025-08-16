226623bf13cbae13b7cd68eb3301c637b53edf3b
Saud <qsaudhussain@gmail.com>
2025-08-16 14:28:41 +0530

    Update Product.js

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,30 +1,3 @@
-// import mongoose from "mongoose";
-
-// const productSchema = new mongoose.Schema({
-//   name: { type: String, required: true },
-//   type: { type: String, required: true },
-//   application: { type: String, required: true },
-//   image: { type: String, required: true },
-//   image2: { type: String }, // New field
-//   materials: {
-//     sealRingFaces: String,
-//     seatFaces: String,
-//     elastomer: String,
-//     moc: String,
-//     bellowMoc: String,
-//     endFittingMoc: String,
-//   },
-//   operatingLimits: {
-//     shaftDia: String,
-//     pressure: String,
-//     temperature: String,
-//     speed: String,
-//   },
-// }, { timestamps: true });
-
-// const Product = mongoose.model("Product", productSchema);
-// export default Product;
-
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
